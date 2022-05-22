7ddf36304afe16cccefe2b73cc7ee71422e2bc07
matthewcroughan <matt@croughan.sh>
2022-05-23 00:44:22 +0100

    Read auth from a file instead of insecure CLI string

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node 
 
 const program = require('commander');
+const fs = require("fs");
 const path = require('path');
 const { handleUpload, handleSubmarine, getSubmarineAuth, getAuth, logIn, logInSubmarine } = require('./lib');
 program
   .version('1.0.3')
   .name('pinata-cli')
   .description('A command line tool to upload files and folders to Pinata')
-  .option('-a, --auth [jwt]', 'API jwt from Pinata')  
-  .option('-as --authSubmarine [api key]', 'V2 API Key from Pinata')
+  .option('-a, --authFile [jwt]', 'API jwt from Pinata')  
+  .option('-as --authFileSubmarine [api key]', 'V2 API Key from Pinata')
   .option('-u, --upload [file or folder]', 'Source folder or file to upload to IPFS')
   .option('-s, --submarine [file or folder]', 'Source folder or file to submarine on Pinata')
   .parse(process.argv)
 
 const main = async () => {
   try {
-    const { upload, submarine, auth, authSubmarine } = program.opts();
+    const { upload, submarine, authFile, authFileSubmarine } = program.opts();
     if(upload) {
       await getAuth();
       const result = await handleUpload(upload);
@@ -28,13 +29,13 @@ const main = async () => {
       console.log(JSON.stringify(result));;
     }
 
-    if(auth) {
-      await logIn(auth);
+    if(authFile) {
+      await logIn(fs.readFileSync(authFile));
       console.log("Authenticated");
     }
 
-    if(authSubmarine) {
-      await logInSubmarine(authSubmarine);
+    if(authFileSubmarine) {
+      await logInSubmarine(fs.readFileSync(authFileSubmarine));
       console.log("Authenticated");
     }
   } catch (error) {
